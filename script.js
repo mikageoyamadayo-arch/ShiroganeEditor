ac7d4a5af00ddcb08c14c9ffeedb2974131cbdd9
おやまみかげ🐤 <mikage.oyamadayo@gmail.com>
2026-07-19 21:09:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function showBook(book){
 
- const detail = document.getElementById("bookDetail");
+const detail = document.getElementById("bookDetail");
 
- const date = books[book];
+const date = books[book];
 
- const chapterList = data.chapters
+const chapterList = data.chapters
     .map(chapter => `<li>${chapter}</li>`)
     .join("");
 
- detail.innerHTML = `
+detail.innerHTML = `
 
- <h2>${data.title}</h2>
+<h2>${data.title}</h2>
 
- <p>${data.volume}</p>
+<p>${data.volume}</p>
 
- <p>${data.description}</p>
+<p>${data.description}</p>
 
- <h3>📖 折一覧</h3>
+<h3>📖 折一覧</h3>
 
- <ul>
- ${chapterList}
- </ul>
+<ul>
+${chapterList}
+</ul>
 
- `;
+`;
     
 }
