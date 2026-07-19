a6bdbc7ea321a020c35a86e1eb2e14a1ceff0a2e
おやまみかげ🐤 <mikage.oyamadayo@gmail.com>
2026-07-19 18:04:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,17 @@
-function showBook(book){
+ffunction showBook(book){
 
-const detail=document.getElementById("bookDetail");
+const detail = document.getElementById("bookDetail");
 
-if(book==="hanaemi"){
+const data = books[book];
 
-detail.innerHTML=`
+detail.innerHTML = `
 
-<h2>🐤 花笑の箒星</h2>
+<h2>${data.title}</h2>
 
-<p>
+<p>${data.volume}</p>
 
-白銀が護りし緋の神子　第0帖。
-
-憩ちゃんの鳥籠時代の物語。
-
-</p>
+<p>${data.description}</p>
 
 `;
-
-}
-
-if(book==="kinka"){
-
-detail.innerHTML=`
-
-<h2>🌸 金霞の五星</h2>
-
-<p>第1帖</p>
-
-<div class="chapter-list">
-
-<button>第1折　結成、特務部隊・白銀</button>
-
-<button>第2折　カフェーという闇</button>
-
-<button>第3折　それぞれの想い</button>
-
-<button>第4折　終わりの始まり</button>
-
-<button>第5折　追うものから追われるものへ</button>
-</div>
-
-`;
-
-}
-  
-if(book==="ginrei"){
-
-detail.innerHTML=`
-
-<h2>❄️ 銀嶺の秘鑰</h2>
-
-<p>
-
-白銀が護りし緋の神子　第2帖。
-
-北領で明かされる真実の物語。
-
-</p>
-
-`;
-
-}
 
 }
