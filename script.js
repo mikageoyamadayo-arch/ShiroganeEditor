230b84c5544f75a270b9418be376fe90f42af671
おやまみかげ🐤 <mikage.oyamadayo@gmail.com>
2026-07-19 17:33:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-// 白銀エディタ
+function showBook(book){
+
+const detail=document.getElementById("bookDetail");
+
+if(book==="hanawarai"){
+
+detail.innerHTML=`
+
+<h2>🐤 花笑の箒星</h2>
+
+<p>
+
+白銀が護りし緋の神子　第0帖。
+
+憩ちゃんの鳥籠時代の物語。
+
+</p>
+
+`;
+
+}
+
+if(book==="kinka"){
+
+detail.innerHTML=`
+
+<h2>🌸 金霞の五星</h2>
+
+<p>
+
+白銀が護りし緋の神子　第1帖。
+
+白銀8人の出会いを描く物語。
+
+</p>
+
+`;
+
+}
+
+if(book==="ginrei"){
+
+detail.innerHTML=`
+
+<h2>❄️ 銀嶺の秘鑰</h2>
+
+<p>
+
+白銀が護りし緋の神子　第2帖。
+
+北領で明かされる真実の物語。
+
+</p>
+
+`;
+
+}
+
+}
