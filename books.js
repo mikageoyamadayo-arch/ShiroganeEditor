0edcaffe5d7b0fe55ca19dd87b76b769e05aac3a
おやまみかげ🐤 <mikage.oyamadayo@gmail.com>
2026-07-19 18:16:14 +0900

    Update books.js

--- books.js
+++ books.js
@@ -1,19 +1,19 @@
 const books = {
 
     hanaemi: {
-        title: "🌼 花笑の`箒星",
+        title: "🐤 花笑の箒星",
         volume: "第0帖",
         description: "白銀が護りし緋の神子、前日譚。"
     },
 
     kinka: {
-        title: "🌺 金霞の五星",
+        title: "🌸 金霞の五星",
         volume: "第1帖",
         description: "白銀が護りし緋の神子、帝都編 第1部。"
     },
 
     ginrei: {
-        title: "　 銀嶺の秘鑰",
+        title: "❄️ 銀嶺の秘鑰",
         volume: "第2帖",
         description: "白銀が護りし緋の神子、北領編 第2部。"
     },
